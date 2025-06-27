d0cdf523939690613eba4d2e3c597c6d3f81ba0f
Susha-123 <111412425+Susha-123@users.noreply.github.com>
2025-06-27 14:39:00 +0530

    Update routes.py

--- order-service/app/routes.py
+++ order-service/app/routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
+
 from . import models, schemas
 from .database import SessionLocal
 
